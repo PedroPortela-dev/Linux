49342c334b3bc9be5350d1fabc6bab1eb47475e4
PedroPortela-dev <pedrocaspop64@gmail.com>
2022-04-13 04:58:21 -0300

    ....

--- ros/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pedroportela/Linux/ros/build/turtlesim/turtlesim__py
-# Build directory: /home/pedroportela/Linux/ros/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
